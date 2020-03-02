894ce3f961eee0fdb76943d4028b7a1438d5edcd
negrahodzic <negra.hodzic@gmail.com>
2020-03-02 10:45:33 +0100

    Dodata klasa ServisMenjacnice koji implementira interfejs IOpercaijeZaKurs

---
+++ Menjacnica/src/bibliotekaKlasa/ServisMenjacnice.java
@@ -0,0 +1,27 @@
+package bibliotekaKlasa;
+
+import java.util.Date;
+
+import operacije.IOperacijeZaKurs;
+
+public class ServisMenjacnice implements IOperacijeZaKurs{
+
+	@Override
+	public void dodajKurs(Date datum, double kurs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbrisiKurs(Date datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs vratiKurs(Date datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
